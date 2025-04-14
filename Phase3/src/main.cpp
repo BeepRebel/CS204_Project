174f2da38ee6b3e9ce6c9a6cbfcef433eea881da
krishn113 <ag.krishna0807@gmail.com>
2025-04-14 19:26:29 +0530

    Initial Commit for Phase3

---
+++ Phase3/src/main.cpp
@@ -0,0 +1,11 @@
+// main.cpp
+// Purpose of this file: This file controls the overall functioning of the Simulator.
+
+#include <bits/stdc++.h>
+#include "myRISCVSim.cpp"
+using namespace std;
+
+int main() {
+
+    return 0;
+}
